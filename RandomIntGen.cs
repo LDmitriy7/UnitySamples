27c4b3b12aa363cd36cf8f2ab32a93842a25ca68
Dmitriy <levchenko.d.a1998@gmail.com>
2025-04-17 01:58:47 +0500

    Update RandomIntGen.cs

--- RandomIntGen.cs
+++ RandomIntGen.cs
@@ -2,13 +2,10 @@ using System;
 using Random = UnityEngine.Random;
 
 [Serializable]
-public class RandomIntGen
+public class RandomIntGen : IntRange
 {
-    public int minValue = 0;
-    public int maxValue = 1;
-    
     public int Gen()
     {
-        return Random.Range(minValue, maxValue + 1);
+        return Random.Range(min, max + 1);
     }
 }
